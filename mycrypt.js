53121c2405a2a578bd339997f2c7c0446dcb8c9d
Jimmy Cao <jc2462@cornell.edu>
2016-04-20 07:57:13 -0400

    Added more util functions

--- mycrypt.js
+++ mycrypt.js
@@ -7,3 +7,25 @@ function xorEncrypt(text, key) {
     }
     return result;
 }
+
+function hammingWeight(i) {
+    i = i - ((i >> 1) & 0x55555555);
+    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+    return ((i + (i >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
+}
+
+function hammingDistNorm(s1, s2) {
+    if(s1.length != s2.length) {
+        throw new Error('Cannot do this when they are differing lengths.');
+    }
+
+    var len = s1.length;
+    var xorResult = xorEncrypt(s1, s2);
+
+    var totalWeight = 0;
+    for(var i = 0; i < len; i++) {
+        totalWeight += hammingWeight(xorResult.charCodeAt(i));
+    }
+    var normWeight = totalWeight / len;
+    return normWeight;
+}
