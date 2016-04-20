7eae316d9e2a3af24712b18be344644378b537c3
Jimmy Cao <jc2462@cornell.edu>
2016-04-20 08:31:22 -0400

    Does this work?

--- autocrack.js
+++ autocrack.js
@@ -1,4 +1,6 @@
 function update() {
+    var MAX_STRIDES = 7;
+
     var text = $("#textbox").val();
     var textlen = text.length;
     var max_key_len = textlen / 2;
@@ -9,10 +11,22 @@ function update() {
         for(var i = 0; i + l < textlen; i += l) {
             var the_substr = text.substring(i, i + l);
             strides.push(the_substr);
-            if(l == 6) console.log(the_substr);
+            if(strides.length >= MAX_STRIDES) {
+                break; 
+            }
         }
         all_strides[l] = strides;
     }
+
+    all_H_dists = {};
+
+    $.each(all_strides, function(len, strides) {
+        for(var i = 0; i < strides.length - 1; i++) {
+            all_H_dists[len] = hammingDistNorm(strides[i], strides[i+1]);
+        }
+    });
+
+    console.log(all_H_dists);
 }
 
 $(function () {
