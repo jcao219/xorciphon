7d143d4490c68c744103b7fe4f83edca898d494f
Jimmy Cao <jc2462@cornell.edu>
2016-04-20 08:24:11 -0400

    Some stuff

---
+++ autocrack.js
@@ -0,0 +1,20 @@
+function update() {
+    var text = $("#textbox").val();
+    var textlen = text.length;
+    var max_key_len = textlen / 2;
+    
+    var all_strides = {};
+    for(var l = 1; l <= max_key_len; l++) {
+        var strides = [];
+        for(var i = 0; i + l < textlen; i += l) {
+            var the_substr = text.substring(i, i + l);
+            strides.push(the_substr);
+            console.log(the_substr);
+        }
+        all_strides[l] = strides;
+    }
+}
+
+$(function () {
+    $("#decrypt_btn").click(update);
+});
